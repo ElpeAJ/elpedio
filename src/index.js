0efdde4e182b036849264f9834aa276f315c0333
Elpedia Junior Arthur <elpediaarthurjunior@gmail.com>
2024-01-20 09:25:50 -0800

    Added API to wind speed, humidity and description of the weather. Also some light changes to make project responsive on phones

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 function changeWeather(response){
   let cityElement = document.querySelector("#weatherAppCity");
-  cityElement.innerHTML = response.data.city;
-
   let actualTemp = response.data.temperature.current;
   let temperatureValueElement = document.querySelector("#weatherAppTemperatureValue");
+  let descriptionElement = document.querySelector("#description");
+  let descriptionResponse = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  let actualHumidity = response.data.temperature.humidity;
+  let windinessElement = document.querySelector("#windiness");
+  let actualWindiness = response.data.wind.speed;
+  cityElement.innerHTML = response.data.city;
   temperatureValueElement.innerHTML = Math.round(actualTemp);
+  descriptionElement.innerHTML = descriptionResponse;
+  humidityElement.innerHTML = `${actualHumidity}%`;
+  windinessElement.innerHTML = `${actualWindiness}km/h`;
+  
+  // console.log(response.data);
 }
 
 function searchCity(city) {
